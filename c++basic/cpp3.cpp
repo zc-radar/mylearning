5bbf27e41e587b9de57813dbad0daea34b746c7e
zc <15572871775@163.com>
2026-09-05 15:31:50 +0800

    规范

--- c++basic/cpp3.cpp
+++ c++basic/cpp3.cpp
@@ -3,7 +3,7 @@
 int main() {
   // Here's some math/physics values that we copy-pasted from elsewhere
   double pi{3.14159};
-  double gravity{9.8};
+  [[maybe_unused]] double gravity{9.8};
   double phi{1.61803};
 
   std::cout << pi << '\n';   // pi is used
